ae012bd4e54abb54ebaad71dbcbded5614ba43ba
Theo Kramer <theo@dns.business>
2023-06-20 11:18:38 +0200

    python2 version - use on old python2 distros

--- epp.py
+++ epp.py
@@ -1,11 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# Sample Python EPP client
-"""
-© Domain Name Services (Pty) Ltd. 2010. All rights reserved.
-© DNS Africa Ltd. 2022. All rights reserved.
-$Id$
-"""
+#!/usr/bin/env python2
 __version__ = "$Id$"
 __author__ = "Ed Pascoe <ed@dnservices.co.za>, David Peall <david@dns.business>, Theo Kramer <theo@dns.business>"
 
@@ -24,17 +17,24 @@ import sys
 import time
 import select
 
-from lib import colorlogging
+# from lib import colorlogging
 
 # From chatgpt https://chat.openai.com - provide python3 code to insert at the start of every function to list the name of the function and the arguments passed to the function
 def log_function_call(func):
     def wrapper(*args, **kwargs):
         if options.testing:
-            print(f"Function Name: {func.__name__}")
-            print(f"Arguments: {args} {kwargs}")
+            # print(f"Function Name: {func.__name__}")
+            print("Function Name: %s" % (func.__name__))
+            # print(f"Arguments: {args} {kwargs}")
+            print("Arguments: %s" % (args, kwargs))
         result = func(*args, **kwargs)
         return result
     return wrapper
+
+# @log_function_call
+# def my_function(arg1, arg2, kwarg1=None, kwarg2=None):
+#     # Function body goes here
+#     pass
 
 # Enable translation
 t = gettext.translation('rye', os.path.join(os.path.dirname(__file__), 'locale'), fallback=True)
@@ -44,6 +44,145 @@ log = logging.getLogger()
 
 packfmt = "!I"
 
+# TK Wed May 31 13:18:08 SAST 2023 FROM /home/coza/regression/EPP/TCPTransport - use TCPTransport class instead of EPPTCPTransport class
+
+# From  /home/coza/regression/EPP/TCPTransport.py
+# vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+def _sslProtoByName(protocol=None):
+  """Returns the ssl protocol number given a protocol name.
+     Correctly handles protocol already being a protocol number.
+     If protocol is None TLSv1 is assumed.
+    
+  """
+  protocol_list = list([x[len('PROTOCOL_'):] for x in dir(ssl) if x.find("PROTOCOL_") == 0 ] ) #Generate a list of protocols from the ssl module.
+  log.debug("Given ssl protocol '%s' is one of %s" % (protocol, ", ".join(protocol_list)))
+
+  if isinstance(protocol, int): #Already converted to a protocol number.
+    return protocol  
+  if protocol is None:
+    return ssl.PROTOCOL_TLSv1
+  try:
+    return getattr(ssl, "PROTOCOL_"+str(protocol))
+  except AttributeError:
+    protocol_list = list([x[len('PROTOCOL_'):] for x in dir(ssl) if x.find("PROTOCOL_") == 0 ] ) #Generate a list of protocols from the ssl module.
+    raise AttributeError("Given ssl protocol '%s' is not and integer or one of %s" % (protocol, ", ".join(protocol_list)))
+
+class TCPTransportError(Exception):
+  """Errors from EPPTCPTransport"""
+
+class TCPTransport:
+  """An epp client transport class. This is just the raw TCP IP protocol. The XML data needs to be handled separately.
+     The EPP transport protocol is defined at http://tools.ietf.org/html/rfc5734 it looks complicated but is 
+     actually very simple and elegant. 
+     the actual data should be XML data formated according to RFC5730-RFC5733
+     No validation of any data takes place in this Class
+  """
+  sock = None
+  sock_host = ""
+  sock_port = 0
+  _greeting = ""
+  _isssl = None
+  _timeout = 5
+
+  def __init__(self, host="127.0.0.1", port=3121, usessl=True, cert=None, nogreeting=False, ssl_version=ssl.PROTOCOL_TLSv1):
+    """Connect to the EPP server. Server header in self.header, """
+    socket.setdefaulttimeout(self._timeout)
+    if usessl:
+      self._isssl = True
+      s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      ssl_version= _sslProtoByName(ssl_version)
+      # do not require a certificate from the server
+      self.sock = ssl.wrap_socket(s, cert_reqs=ssl.CERT_NONE, certfile=cert, ssl_version=ssl_version)
+      try:
+        self.sock.connect((host, port))
+      except:
+        print "Failed to connect to EPP server %s on port %s. Is the server off line?" % (host, port)
+        sys.exit(1)
+    else:
+      self._isssl = False
+      self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      self.sock.connect((host, port))
+    if not nogreeting:
+      self._greeting = self.get()
+
+    self.sock_host = host
+    self.sock_port = port
+    log.debug("Connected to server '%s:%d'" % (self.sock_host, self.sock_port))
+
+  def get(self):
+    """Get an EPP response """
+    header = ""
+    rest =  "" #If we get more bytes than we expected.
+    
+    self.sock.settimeout(self._timeout)
+    self.sock.setblocking(False)
+    tries = 5
+    loopCount = 0
+
+    while len(header) < 4:
+      if self._isssl:
+        data = self.sock.recv(4)
+      else:
+        data = self.sock.recv(4, 0x40) #0x40 is the MSG_WAITALL flag which can only be used on plain sockets
+
+      if len(data) == 0:
+        if loopCount >= tries:
+          raise TCPTransportError(_("Did not receive anything from server '%s:%d' or socket timeout. Was the initial login invalid?" % (self.sock_host, self.sock_port)))
+        else:
+          time.sleep(0.5) # sleep 500 millisecs
+          loopCount += 1
+          continue
+        
+      header = header + data
+      if len(header) > 4: 
+        rest = header[4:]
+        header = header[:4]
+
+    bytes1 = struct.unpack(packfmt,header)[0]
+
+    if self._isssl:
+      data = [] #the buffer
+      total = rest #Initialize with anything extra read while we were getting the header.
+      while len(total) < (bytes1-4):
+        length = self.sock.recv_into(data, 16384)
+        if length == 0:
+          # Are the next 2 necessary ?
+          # self.sock.settimeout(None)
+          # self.sock.setblocking(True)
+          raise TCPTransportError(_("Could not receive the rest of the expected message header. Only have {0} bytes out of expected {1}.").format(len(total), (bytes1-4)))
+
+        total += "".join(data[:length])
+    else:
+      total = self.sock.recv((bytes1-4), 0x40)
+
+    # Are the next 2 necessary ?
+    # self.sock.settimeout(None)
+    # self.sock.setblocking(True)
+    return total # String - bad choice of var name
+
+  def send(self, data):
+    """Send an EPP command """
+    #Write the header
+    self.sock.send(struct.pack(packfmt, int(len(data)+4)))
+    #Send the data
+    self.sock.send(data)
+
+  def request(self, data):
+    """Convenience function. Does a send and then returns the result of a get"""
+    self.send(data)
+    return self.get()
+
+  def getGreeting(self):
+    """Returns the EPP servers greeting"""
+    return self._greeting
+
+  def close(self):
+    """Close the socket"""
+    log.debug("Socket close requested")
+    self.sock.close()
+
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# From  /home/coza/regression/EPP/TCPTransport.py - replaces the following
 
 class EPPTCPTransport:
     """An epp client transport class. This is just the raw TCP IP protocol. The XML data needs to be handled separatly.
@@ -58,13 +197,22 @@ class EPPTCPTransport:
 
     def __init__(self, host="127.0.0.1", port=3121, usessl=True, cert=None, nogreeting=False):
         """Connect to the EPP server. Server header in self.header"""
+        log.debug("EPPTCPTransport.__init__ (host = %s, port = %d, usessl = %s, cert = %s, nogreeting = %s)", host, port, usessl, cert, nogreeting)
         if usessl:
             self._isssl = True
-            context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+            # TK Wed May 31 12:30:20 SAST 2023 - https://stackoverflow.com/questions/28228214/ssl-module-object-has-no-attribute-sslcontext
+            # context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+            context = ssl.create_default_context()
+            context.check_hostname = False
             if cert:
                 context.load_cert_chain(cert) #'/u/elp/test/epp/dnservices.pem') #certfile="cert.crt", keyfile="cert.key")
+            else: # TK Wed May 31 12:30:20 SAST 2023 - 0x40 causes
+                context.verify_mode=ssl.CERT_NONE
             self.sock = context.wrap_socket(socket.socket(socket.AF_INET))
-            self.sock.connect((host, port))
+        else:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        self.sock.connect((host, port))
 
         if not nogreeting:
             self._greeting = self.get()
@@ -77,10 +225,21 @@ class EPPTCPTransport:
         while len(header) < 4:
             if self._isssl:
                 data = self.sock.recv(4)
-                #log.debug("Initial header via TLS. (%s), %s len %s", type(data), data, len(data))
+                log.debug("Initial header via TLS. (%s), %s len %s", type(data), data, len(data))
             else:
-                data = self.sock.recv(4, 0x40)  # 0x40 is the MSG_WAITALL flag which can only be used on plain sockets
-                #log.debug("Initial header no encryption. (%s), %s len %s", type(data), data, len(data))
+                # TK Wed May 31 12:30:20 SAST 2023 - 0x40 causes
+                #
+                # Traceback (most recent call last):
+                #   File "/home/theo/bin/epp2.py", line 274, in <module>
+                #     epp = EPPTCPTransport(options.host, int(options.port), not options.nossl, nogreeting=options.nogreeting)
+                #   File "/home/theo/bin/epp2.py", line 73, in __init__
+                #     self._greeting = self.get()
+                #   File "/home/theo/bin/epp2.py", line 85, in get
+                #     data = self.sock.recv(4, 0x40)  # 0x40 is the MSG_WAITALL flag which can only be used on plain sockets
+                # socket.error: [Errno 11] Resource temporarily unavailable
+                # data = self.sock.recv(4, 0x40)  # 0x40 is the MSG_WAITALL flag which can only be used on plain sockets
+                data = self.sock.recv(4)  # 0x40 is the MSG_WAITALL flag which can only be used on plain sockets
+                log.debug("Initial header no encryption. (%s), %s len %s", type(data), data, len(data))
             if len(data) == 0:
                 print("<!-- " + _(
                     "Did not receive anything from the server or socket timeout. Was the initial login invalid?") + " -->")
@@ -109,7 +268,8 @@ class EPPTCPTransport:
             total = total + data
         return str(total,'utf-8')
 
-    def send(self, data: bytes):
+    # def send(self, data: bytes):
+    def send(self, data):
         """Send an EPP command """
         # Write the header
         self.sock.write(struct.pack('=L', socket.htonl(len(data) + 4)))
@@ -215,15 +375,16 @@ def fileRead(fname):
 if __name__ == "__main__":
     usage = (_("Usage:") + " %prog [<options>] <files...>\n" +
              _("Example EPP client. The individual EPP commands should be in files specified on the command line.\n")
-             + _("Eg: ./epp.py --host=reg-test.dnservices.co.za login.xml create_host.xml create_domain.xml\n")
+             + _("Eg: ./epp2.py --host=reg-test.dnservices.co.za login.xml create_host.xml create_domain.xml\n")
+             + _("or: cat create_domain.xml | ./epp2.py --host=reg-test.dnservices.co.za -p 3121 -u uniforum --password=PASSWORD  -d DOMAIN=uniforum.co.za # where '%(DOMAIN)s' in create_domain.xml is replaced with 'uniforum.co.za' and the login is handled internally using EPP login credentials defined by '-u' and '--password' \n")
              + _("Will replace all occurances of __CLTRID__  with a suitable clTRID value\n"))
-    if sys.version_info[0] >= 2 and sys.version_info[1] >= 6:
-        usage = usage + __doc__
+    # if sys.version_info[0] >= 2 and sys.version_info[1] >= 6:
+    #     usage = usage + __doc__
     # print usage
     # sys.exit(1)
     parser = optparse.OptionParser(usage)
     parser.add_option("--host", "--ip", dest="host", default="127.0.0.1", help=_("Host to connect to [%default] "))
-    parser.add_option("--port", "-p", dest="port", default="8443", help=_("Port to connect to") + " [%default]")
+    parser.add_option("--port", "-p", dest="port", default="3121", help=_("Port to connect to") + " [%default]")
     parser.add_option("--cert", "-c", dest="cert", help=_("SSL certificate to use for authenticated connections"))
     parser.add_option("--nossl", dest="nossl", action="store_true", default=False, help=_("Do not use SSL"))
     parser.add_option("--verbose", "-v", dest="verbose", action="store_true", default=False,
@@ -242,10 +403,10 @@ if __name__ == "__main__":
 
     if options.verbose:
         logging.basicConfig(level=logging.DEBUG)
-        colorlogging.enableLogging(debug=True, color=True, console=True)
+        # colorlogging.enableLogging(debug=True, color=True, console=True)
     else:
         logging.basicConfig(level=logging.INFO)
-        colorlogging.enableLogging(debug=False, color=True, console=True)
+        # colorlogging.enableLogging(debug=False, color=True, console=True)
 
     # Args not necessary if piping an EPP xml command into epp.py
     inputFromStdin = False
@@ -264,10 +425,13 @@ if __name__ == "__main__":
                 except IOError as e:
                     print("Could not read the SSL certificate %s\n%s" % (options.cert, e))
                     sys.exit(1)
-                epp = EPPTCPTransport(options.host, int(options.port), usessl=not options.nossl, cert=options.cert,
-                                      nogreeting=options.nogreeting)
+                # epp = EPPTCPTransport(options.host, int(options.port), usessl=not options.nossl, cert=options.cert,
+                #                       nogreeting=options.nogreeting)
+                epp = TCPTransport(options.host, int(options.port), usessl=not options.nossl, cert=options.cert,
+                                   nogreeting=options.nogreeting)
             else:
-                epp = EPPTCPTransport(options.host, int(options.port), not options.nossl, nogreeting=options.nogreeting)
+                # epp = EPPTCPTransport(options.host, int(options.port), not options.nossl, nogreeting=options.nogreeting)
+                epp = TCPTransport(options.host, int(options.port), not options.nossl, nogreeting=options.nogreeting)
         except ssl.SSLError as e:
             print("Could not connect due to an SSL error")
             print(e)
